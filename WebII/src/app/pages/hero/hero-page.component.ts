2493616b26b4ae7a991006cf4e187cb148e54dab
valemboo <valeriort08@gmail.com>
2026-05-29 12:54:48 -0600

    Semana 3 tarea

--- WebII/src/app/pages/hero/hero-page.component.ts
+++ WebII/src/app/pages/hero/hero-page.component.ts
@@ -7,15 +7,23 @@ import { Component, signal } from "@angular/core";
 export class heroPageComponent{
     name = signal('Harley Quinn');
     age = signal('28 años');
+    description = signal('');
 
-    changeInfo(){
+    changeName(){
         this.name.set('Wonder Woman');
+    }
+    changeAge(){
         this.age.set('30 años');
     }
     getHeroDescription(){
-
+        if (this.name() == 'Harley Quinn'){
+            return('Harley Quinn era una psiquiatra llamada Harleen Quinzel. Se volvió una villana/antiheroína muy impredecible, divertida y experta usando mazos y bates.');
+        } else{
+            return('Diana Prince es una guerrera amazona con súper fuerza, velocidad y habilidades de combate. Usa el Lazo de la Verdad y lucha por la paz y la justicia.');
+        }
     }
     reset(){
-
+        this.name.set('Harley Quinn')
+        this.age.set('28 años')
     }
 }
