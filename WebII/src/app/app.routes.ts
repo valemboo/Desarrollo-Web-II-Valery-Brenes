5bef70dd4056bcd070e29e045a11d7c7cf95bf37
valemboo <valeriort08@gmail.com>
2026-05-28 20:34:07 -0600

    Indicación de rutas

--- WebII/src/app/app.routes.ts
+++ WebII/src/app/app.routes.ts
@@ -1,3 +1,13 @@
+// Archivo para enlazar cada uno de los archivos, indicar las rutas
 import { Routes } from '@angular/router';
+// Importar componente
+import { CounterPageComponent } from './pages/counter/counter.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    //Cada posición del arreglo indica un objeto
+    {
+        //Path indica que se le va a agregar a la dirección de dominio
+        path:'',
+        component: CounterPageComponent, //cargar cuando es la url inicial
+    }
+];
---
+++ WebII/src/app/pages/counter/counter.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core"; // Trabajar con la librería
+
+@Component({ //Enlazarlo
+    templateUrl:'./counter.component.html', // Indicar qué quiero mostrar
+    styleUrl: './counter.component.css' // Indicar la estética
+})
+export class CounterPageComponent{
+
+}
