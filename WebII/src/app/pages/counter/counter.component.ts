ede7c8e1c81c38ee28f33cd01b8770f10ceb3ecc
valemboo <valeriort08@gmail.com>
2026-05-28 21:31:33 -0600

    Semana 3

--- WebII/src/app/pages/counter/counter.component.ts
+++ WebII/src/app/pages/counter/counter.component.ts
@@ -1,9 +1,19 @@
-import { Component } from "@angular/core"; // Trabajar con la librería
+import { Component, signal } from "@angular/core"; // Importar librería. Trabajar con componentes y señales
 
 @Component({ //Enlazarlo
     templateUrl:'./counter.component.html', // Indicar qué quiero mostrar
     styleUrl: './counter.component.css' // Indicar la estética
 })
 export class CounterPageComponent{
-
+    contador = 10;
+    contadorSignal = signal(5); // (Indica el valor inicial)
+    // Método recibe parámetro tipo numérico
+    increaseBy(value:number){
+        this.contador += value;
+        this.contadorSignal.set(this.contadorSignal()+value); //para señal
+    }
+    reset(){ //restablecer valores
+        this.contador = 10;
+        this.contadorSignal.set(5);
+    }
 }
