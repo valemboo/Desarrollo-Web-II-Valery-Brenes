6ec3e205fdf9ef40e6202fa11732c9be4fad2d89
valemboo <valeriort08@gmail.com>
2026-07-09 20:12:43 -0600

    Cambios al character-list

--- WebII/src/app/components/character-list/character-list.component.ts
+++ WebII/src/app/components/character-list/character-list.component.ts
@@ -1,5 +1,5 @@
-import { Component,output,signal } from "@angular/core";
-//import { Character } from "../../interfaces/character.interface"; //importar tipo Character de interfaces
+import { Component,input } from "@angular/core";
+import { Character } from "../../interfaces/character.interface"; //importar tipo Character de interfaces
 
 @Component({
     selector: 'app-character-list',
@@ -8,5 +8,8 @@ import { Component,output,signal } from "@angular/core";
 })
 
 export class CharacterListComponent{
-    
+    // de dónde vienen, son salidas de información
+    characters = input.required<Character[]>() //debe de cumplir con los elementos de Character (id, power...)
+    listName = input.required<string>() //required obliga a mandar la información si o si
+    //dos formas distintas de enviar la información
 }
